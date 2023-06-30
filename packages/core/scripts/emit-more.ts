6e7eb4b6481282506a0acf8da8904a7a8834d4ab
Aniket Chowdhury <8.aniket.chowdhury@gmail.com>
2023-06-30 06:52:41 +0530

    feat: emit

--- packages/core/scripts/emit-more.ts
+++ packages/core/scripts/emit-more.ts
@@ -1,15 +1,25 @@
-// import { deployments, ethers, getNamedAccounts } from "hardhat";
+import { deployments, ethers, getNamedAccounts } from "hardhat";
 
-// import { ERC20PresetFixedSupply } from "../typechain-types";
+import {
+  ERC20PresetFixedSupply,
+  ERC721PresetMinterPauserAutoId,
+} from "../typechain-types";
 
-// const runner = async () => {
-//   const WETH = await ethers.getContract<ERC20PresetFixedSupply>("WrappedEther");
-//   const USDT = await ethers.getContract<ERC20PresetFixedSupply>("Tether");
+const runner = async () => {
+  const WETH = await ethers.getContract<ERC20PresetFixedSupply>("WrappedEther");
+  const USDT = await ethers.getContract<ERC20PresetFixedSupply>("Tether");
+  const TestNFT = await ethers.getContract<ERC721PresetMinterPauserAutoId>(
+    "TestNFT"
+  );
 
-//   const { admin } = await getNamedAccounts();
+  const { admin, deployer } = await getNamedAccounts();
 
-//   WETH.transfer(admin, ethers.utils.parseEther("100000"));
-//   USDT.transfer(admin, ethers.utils.parseEther("100000"));
-// };
+  await WETH.transfer(admin, ethers.utils.parseEther("100000"));
+  await USDT.transfer(admin, ethers.utils.parseEther("100000"));
 
-// runner().catch(console.error);
+  for (let index = 0; index < 5; index++) {
+    await TestNFT.mint(deployer);
+  }
+};
+
+runner().catch(console.error);
